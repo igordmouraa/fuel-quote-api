38899c209fd79f158fd238bb1fd9e9ceb4395dcb
igordmouraa <igordmoura_@hotmail.com>
2025-04-25 21:33:22 -0300

    feat:initialize NestJS project with gas station management API

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fuel Quote API')
+    .setDescription('API para gerenciamento de cotações de combustíveis')
+    .setVersion('1.0')
+    .addTag('fuel')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(3000);
+}
+
+bootstrap();
