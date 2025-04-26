886e35977a8459776aa160290a6cfb3f79a40f16
igordmouraa <igordmoura_@hotmail.com>
2025-04-26 11:00:57 -0300

    feat: configure global environment variables and add modules for database and gas station management

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { GasStationModule } from './gas-station/gas-station.module';
+import { QuotationModule } from './quotation/quotation.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.development',
+    }),
+    DatabaseModule,
+    GasStationModule,
+    QuotationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
