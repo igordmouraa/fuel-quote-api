6079360e1b6bae010d3cca5ab36daad62dd8bdaa
igordmouraa <igordmoura_@hotmail.com>
2025-04-26 11:00:36 -0300

    feat: enable CORS for the Fuel Quote API

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Fuel Quote API')
     .setDescription('API para gerenciamento de cotações de combustíveis')
