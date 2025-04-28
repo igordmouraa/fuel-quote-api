5b88817a3add9927085887183024a75d19c9a8a2
igordmouraa <igordmoura_@hotmail.com>
2025-04-28 00:06:33 -0300

    feat: integrate TerminusModule for health checks and update environment file path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { GasStationModule } from './gas-station/gas-station.module';
 import { QuotationModule } from './quotation/quotation.module';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env.development',
+      envFilePath: '.env',
     }),
     DatabaseModule,
     GasStationModule,
     QuotationModule,
+    TerminusModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
